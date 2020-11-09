37a1f1f08da2e4bddc2e7bb08a22cdbf08116a3d
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-11-09 15:20:13 -0300

    Initial commit

---
+++ SO_T1/SO_T1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace SO_T1
+{
+    class CPU
+    {
+        public int counter; // registrador | o contador de programa
+        public int accumulator; // registrador | acumulador
+        public bool status;// codigo de interrupcao
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C://teste/SO.txt";
+
+            CPU cpu = new CPU();
+
+            string[] programMemory = new string[10]; // memoria de programa
+            int[] dataMemory = new int[10]; // memoria de dados
+
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                programMemory = content.Split("\n");
+            }
+            else
+            {
+                Console.WriteLine("Path " + path + " was not found.");
+            }
+
+            for (int i = 0; i < programMemory.Length; i++)
+            {
+                Console.WriteLine(i + " : " + programMemory[i]);
+            }
+        }
+    }
+}
