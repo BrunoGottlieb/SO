34e6ec2cd1fb129591a1a945c091d46837760cfd
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2020-11-17 01:52:11 -0300

    16-11-20 - Part I completed, using all methods

--- SO_T1/SO_T1/Program.cs
+++ SO_T1/SO_T1/Program.cs
@@ -14,9 +14,9 @@ namespace SO_T1
 
     class Status
     {
-        public int PC; // registrador | o contador de programa
-        public int A; // registrador | acumulador
-        public int interruptionCode;// codigo de interrupcao // 0 normal | 1 instrucao ilegal | 2 violacao de memoria
+        public int PC { get; set; } // registrador | o contador de programa
+        public int A { get; set; } // registrador | acumulador
+        public int InterruptionCode { get; set; }// codigo de interrupcao // 0 normal | 1 instrucao ilegal | 2 violacao de memoria
     }
 
     class Program
@@ -24,41 +24,43 @@ namespace SO_T1
         public const int normal = 0;
         public const int ilegal = 1;
         public const int violacao = 2;
+
         public const bool debugMode = true;
+
 
         static void Main(string[] args)
         {
-            string path = "C://teste/SO.txt";
-            int[] dados = new int[4];
+            string path = "C://teste/SO.txt"; // diretorio
+            int[] dados = new int[4]; // // dados do programa
 
-            CPU cpu = new CPU();
-            Status status = new Status();
+            CPU cpu = new CPU(); // instancia da CPU
+            Status status = new Status(); // instancia dos status do CPU
 
-            if (File.Exists(path))
+            if (File.Exists(path)) // confere se o diretorio existe e le o seu conteudo
             {
                 string content = File.ReadAllText(path);
                 string[] array = content.Split("\n");
-                cpu_altera_programa(cpu, array);
+                SetCPUProgramMemory(cpu, array); // manda as instrucoes para a cpu
             }
             else
             {
                 Console.WriteLine("Path " + path + " was not found.");
             }
 
-            for (int i = 0; i < cpu.programMemory.Length; i++)
+            for (int i = 0; i < cpu.programMemory.Length; i++) // exibe o conteudo da memoria de programa [instrucoes]
             {
                 Console.WriteLine(i + " : " + cpu.programMemory[i]);
             }
 
-            cpu_estado_inicializa(status); // inicializa o estado da cpu
+            InitializeCPU(status); // inicializa o estado da cpu
 
-            cpu_altera_estado(cpu, status); // altera o estado da cpu
+            UpdateCPUStatus(cpu, status); // altera o estado da cpu
 
-            cpu_altera_dados(cpu, dados);
+            SetCPUDataMemory(cpu, dados); // envia os dados para a cpu
 
-            while (cpu.status.interruptionCode == normal)
+            while (GetCPUInterruptionCode(cpu) == normal)
             {
-                cpu_executa(cpu);
+                ExecuteCPU(cpu); // executa as instrucoes caso a cpu esteja em estado normal
             }
 
             if(debugMode)
@@ -71,70 +73,71 @@ namespace SO_T1
         }
 
         // alterar o conteúdo da memória de programa (recebe um vetor de strings)
-        static void cpu_altera_programa(CPU cpu, string[] newData)
+        static void SetCPUProgramMemory(CPU cpu, string[] newData)
         {
             cpu.programMemory = newData;
         }
 
         // alterar o conteúdo da memória de dados (recebe um vetor de inteiros, que é alterado pela execução das instruções)
-        static void cpu_altera_dados(CPU cpu, int[] newData)
+        static void SetCPUDataMemory(CPU cpu, int[] newData)
         {
             cpu.dataMemory = newData;
         }
 
         // obter o conteúdo da memória de dados (retorna um vetor de inteiros que é o conteúdo atual da memória – não precisa desta função caso o vetor passado pela função acima seja alterado “in loco”)
-        static int[] cpu_salva_dados(CPU cpu)
+        static int[] GetCPUDataMemory(CPU cpu)
         {
             return cpu.dataMemory;
         }
 
         // ler o modo de interrupção da CPU (normal ou um motivo de interrupção)
-        static int cpu_interrupcao(CPU c)
+        static int GetCPUInterruptionCode(CPU c)
         {
-            return c.status.interruptionCode;
+            return c.status.InterruptionCode;
         }
 
         // colocar a CPU em modo normal (coresponde ao retorno de interrupção) – muda para modo normal e incrementa o PC; se já estiver em modo normal, não faz nada
-        static int cpu_retorna_interrupcao(CPU cpu)
+        static int GetCPUInterruptionMode(CPU cpu)
         {
-            return cpu.status.interruptionCode;
+            return cpu.status.InterruptionCode;
         }
 
         // obter a instrução em PC (que pode ser inválida se PC estiver fora da memória de programa)
-        static string cpu_instrucao(CPU cpu)
+        static string GetPCInstruction(CPU cpu)
         {
             return cpu.programMemory[cpu.status.PC];
         }
 
         // obter o estado interno da CPU (retorna o valor de todos os registradores)
-        static void cpu_salva_estado(CPU cpu, Status e)
+        static Status GetCPUStatus(CPU cpu)
         {
-            e = cpu.status;
+            return cpu.status;
         }
 
         // alterar o estado interno da CPU (copia para os registradores da cpu os valores recebidos)
-        static void cpu_altera_estado(CPU cpu, Status e)
+        static void UpdateCPUStatus(CPU cpu, Status e)
         {
             cpu.status = e;
         }
 
         // inicializar o estado interno da CPU (PC=0, A=0, estado=normal)
-        static void cpu_estado_inicializa(Status e)
+        static void InitializeCPU(Status e)
         {
             e.PC = 0;
             e.A = 0;
-            e.interruptionCode = normal; // normal
+            e.InterruptionCode = normal; // normal
         }
 
         // executar uma instrução (só executa se estiver em modo normal)
-        static void cpu_executa(CPU cpu)
+        static void ExecuteCPU(CPU cpu)
         {
-            if(cpu_retorna_interrupcao(cpu) == normal)
+            if(GetCPUInterruptionMode(cpu) == normal)
             {
-                bool updatePC = true;
+                bool updatePC = true; // controla se o PC devera ser atualizado ao terminar a execucao da instrucao
 
-                int PC = cpu.status.PC; // pega o valor atual de PC
-                string origem = cpu.programMemory[PC];
+                Status status = GetCPUStatus(cpu);
+
+                string origem = GetPCInstruction(cpu); // retorna a instrucao atual do PC
 
                 string[] instrucao = origem.Split(' ');
                 string instruction = instrucao[0];
@@ -153,56 +156,66 @@ namespace SO_T1
 
                 if (instruction == "CARGI") // coloca o valor n no acumulador (A=n)
                 {
-                    cpu.status.A = value;
-                    Console.WriteLine(cpu.status.A);
+                    status.A = value;
                 }
 
                 else if (instruction == "CARGM") // coloca no acumulador o valor na posição n da memória de dados (A=M[n])
                 {
-                    cpu.status.A = cpu.dataMemory[value];
-                    Console.WriteLine(cpu.status.A);
+                    int[] data = GetCPUDataMemory(cpu);
+                    status.A = data[value];
                 }
 
                 else if (instruction == "CARGX") // coloca no acumulador o valor na posição que está na posição n da memória de dados (A=M[M[n]])
                 {
-                    cpu.status.A = cpu.dataMemory[cpu.dataMemory[value]];
-                    Console.WriteLine(cpu.status.A);
+                    int[] data = GetCPUDataMemory(cpu);
+                    int pos = data[value];
+                    status.A = pos;
+                    //status.A = cpu.dataMemory[cpu.dataMemory[value]];
                 }
 
                 else if (instruction == "ARMM") // coloca o valor do acumulador na posição n da memória de dados (M[n]=A)
                 {
-                    cpu.dataMemory[value] = cpu.status.A;
+                    int[] data = GetCPUDataMemory(cpu);
+                    data[value] = status.A;
+                    SetCPUDataMemory(cpu, data);
+                    //cpu.dataMemory[value] = cpu.status.A;
                 }
 
                 else if (instruction == "ARMX") // 	coloca o valor do acumulador posição que está na posição n da memória de dados (M[M[n]]=A)
                 {
-                    cpu.dataMemory[cpu.dataMemory[value]] = cpu.status.A;
+                    int[] data = GetCPUDataMemory(cpu);
+                    int pos = data[value];
+                    data[pos] = status.A;
+                    SetCPUDataMemory(cpu, data);
+                    //cpu.dataMemory[cpu.dataMemory[value]] = cpu.status.A;
                 }
 
                 else if (instruction == "SOMA") // 	soma ao acumulador o valor no endereço n da memória de dados (A=A+M[n])
                 {
-                    cpu.status.A = cpu.status.A + cpu.dataMemory[value];
+                    status.A = status.A + cpu.dataMemory[value];
                 }
 
                 else if (instruction == "NEG") // 	inverte o sinal do acumulador (A=-A)
                 {
-                    cpu.status.A *= -1;
+                    status.A *= -1;
                 }
 
                 else if (instruction == "DESVZ") // se A vale 0, coloca o valor n no PC
                 {
-                    if (cpu.status.A == 0)
+                    if (status.A == 0)
                     {
-                        cpu.status.PC = value;
+                        status.PC = value;
                         updatePC = false;
                     }
                 }
 
                 else // coloca a CPU em interrupção – instrução ilegal
                 {
-                    cpu.status.interruptionCode = ilegal;
+                    status.InterruptionCode = ilegal;
                     updatePC = false;
                 }
+
+                UpdateCPUStatus(cpu, status); // atualiza o estado da CPU com os novos dados
 
                 if (updatePC) { cpu.status.PC++; }
 
