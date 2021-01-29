a9f3bdffa85a77c9b07039c63cd06463cc2e4706
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2021-01-29 18:08:41 -0300

    29-01-21 - Added Memory class

---
+++ SO_T2/SO_T2/Memory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO_T2
+{
+    class Memory
+    {
+        static int[] memory; // memoria fisica
+        static int pageSize = 10; // tamanho da pagina
+        static int totalMemorySize = 1000; // tamanho total da memoria
+        static int frameSize = pageSize; // tamanho do quadro
+
+        // obter o tamanho total da memória 
+        public static int getTotalMemorySize()
+        {
+            return totalMemorySize;
+        }
+
+        // obter o tamanho de um quadro
+        public static int getFrameSize()
+        {
+            return frameSize;
+        }
+
+        // ler um inteiro de uma posição da memória;
+        public static int readContentFromMemoryPosition(int index)
+        {
+            return memory[index];
+        }
+
+        // alterar um inteiro de uma posição da memória;
+        public static void updateMemoryContentAtIndex(int newData, int index)
+        {
+            memory[index] = newData;
+        }
+
+        // ler o conteúdo de um quadro da memória;
+        public static int getContentFromMemoryPosition(int index)
+        {
+            return memory[index];
+        }
+
+        // alterar o conteúdo de um quadro da memória.
+        public static void setMemoryFrameAtIndex(int newData, int index)
+        {
+
+        }
+    }
+}
